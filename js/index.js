8f82937949a7cf5181e8b93260fbfc9ea2e7a54a
SunmoonL <dltjsans123@naver.com>
2022-12-07 18:02:32 +0900

    22.12.07

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.small i:nth-of-type(1)').click(function(){
+        $('#myNav').css('width','100%');
+    })
+    $('.closebtn').click(function(){
+        $('#myNav').css('width','0');
+    });
+});
+
