da9fc55af9baa3b0ac9c61651f6b24b6854afb5d
SunmoonL <dltjsans123@naver.com>
2022-12-08 17:56:22 +0900

    video

--- js/index.js
+++ js/index.js
@@ -1,9 +1,31 @@
+// 햄버거 nav
 $(document).ready(function(){
     $('.small i:nth-of-type(1)').click(function(){
         $('#myNav').css('width','100%');
     })
     $('.closebtn').click(function(){
         $('#myNav').css('width','0');
+    });
+});
+
+//최신음악
+$(function(){
+    $('.selectsong1').click(function(){
+        $('.song1').show();
+        $('.song2').hide();
+        $('.song3').hide();
+    });
+
+    $('.selectsong2').click(function(){
+        $('.song1').hide();
+        $('.song2').show();
+        $('.song3').hide();
+    });
+
+    $('.selectsong3').click(function(){
+        $('.song1').hide();
+        $('.song2').hide();
+        $('.song3').show();
     });
 });
 
