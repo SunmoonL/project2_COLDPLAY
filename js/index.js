df072913b3f3cc74a9385094b032d2b9a5f80327
SunmoonL <dltjsans123@naver.com>
2022-12-13 15:47:09 +0900

    22.12.13

--- js/index.js
+++ js/index.js
@@ -11,18 +11,18 @@ $(document).ready(function(){
 //slide nav
 $(function(){
     $('nav').mouseenter(function(){
-        $('nav > ul > li > div').slideDown();
-        $('.navlist > div > ul > li > div > a').css('color','black');
+        $('nav > ul > li > div').stop().slideDown();
         $('.small > nav > ul > li > a').css('color','black');
         $('.small > i').css('color','black');
-
+        $('.navdown').css('height','300px');
     });
 
      $('nav').mouseleave(function(){
-        $('nav > ul > li > div').slideUp();
         $('.navlist > div > ul > li > div > a').css('color','#fff');
         $('.small > nav > ul > li > a').css('color','#fff');
         $('.small > i').css('color','#fff');
+        $('.navdown').css('height','0');
+        $('nav > ul > li > div').stop().slideUp("fast");
     });
 });
 
